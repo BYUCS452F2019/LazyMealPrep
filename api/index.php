bc5970c4b2b80594ab5122aff8d77ec259f0383a
Cameron Conradt <cameronconradt@gmail.com>
2019-10-21 09:26:40 -0600

    test php config

---
+++ api/index.php
@@ -0,0 +1,16 @@
+<?php
+
+$DATABASE_URL = 'sql-meal.cndjbcddhtpq.us-east-2.rds.amazonaws.com';
+
+
+print('Success!');
+if(!empty($GET)){
+    switch($GET['table']){
+        case 'account':
+
+    }
+}
+
+else if(!empty($POST)){
+
+}

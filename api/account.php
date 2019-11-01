865b866e2010fe1fadc71ddb082eea9142b5fe6e
Cameron Conradt <cameronconradt@gmail.com>
2019-10-31 21:43:14 -0600

    debug

--- api/account.php
+++ api/account.php
@@ -12,6 +12,7 @@ try {
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $json = file_get_contents('php://input');
         $data = json_decode($json, true);
+        echo $data['type'];
         switch ($data['type']) {
             case 'new':
                 if (!empty($data['username']) && !empty($data['email']) && !empty($data['password'])) {
